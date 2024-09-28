bbce4f5454d3e4d064388284c821caed5ce477eb
rimsha_shehzadi <rimshashehzadi79@gmail.com>
2024-09-28 13:58:42 +0500

    react

---
+++ react_app/src/app/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = (props:any) => {
+  return (
+    <div>
+        <h1>This is Header Page {props.name}</h1>
+        
+    </div>
+  )
+}
+
+export default Header
--- react_app/src/app/page.tsx
+++ react_app/src/app/page.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import Header from './components/Header'
 
 const page = () => {
+  const name = "Rimsha"
   return (
-    <div>page</div>
+    <div>
+    {/* // MORE TEN ONE ELEMENTS IN  RACT Fragment */}
+    <div>
+<h1 className='text-xl font-extrabold'>Helo React! I'm {name} startimg this course</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod facere corrupti magnam dignissimos consectetur ipsam corporis, sint minima minus eum placeat omnis non culpa officiis, illo dolorem soluta ducimus atque.</p>
+{/* first componenets */}
+<Header name="Fatima"/>
+<Header name="Razia"/>
+<Header name="Amina"/>
+<Header name="Maryam"/>
+      
+    </div>
+    </div>
   )
 }
 
