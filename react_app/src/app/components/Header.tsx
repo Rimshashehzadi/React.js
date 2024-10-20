9b3a5d120ad0cea6f316e65d912e5528509d159e
rimsha_shehzadi <rimshashehzadi79@gmail.com>
2024-10-20 20:44:06 +0500

    reactjs

--- react_app/src/app/components/Header.tsx
+++ react_app/src/app/components/Header.tsx
@@ -4,6 +4,7 @@ const Header = () => {
   return (
     <div>
         <h1 className='text-center'>This is Header Page </h1>
+        <h1 className='text-center'>This is Footer Page </h1>
         
     </div>
   )
